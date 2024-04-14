d5aa5611fb45dcec7b93446f2bea97152fb276ba
missjini <kimhero91@naver.com>
2024-04-14 22:55:40 +0900

    초기 세팅 입니다.

---
+++ footer.php
@@ -0,0 +1,25 @@
+    <div class="footer">
+        <div class="company_info fc_58">
+            <div class="f16 fw_700">DEEPTAIL</div>
+            <div class="f14">대표: 이지현</div>
+            <div class="f14">TEL: 062-000-0000</div>
+            <div class="f14">E-MAIL: deeptail99@naver.com</div>
+        </div>
+        <div class="sns_wrapper">
+            <a href="">
+                <div class="sns_icon">
+                    <img src="assets/_icon/kakao.jpg" />
+                </div>
+            </a>
+            <a href="">
+                <div class="sns_icon">
+                    <img src="assets/_icon/naverblog.jpg" />
+                </div>
+            </a>
+            <a href="">
+                <div class="sns_icon">
+                    <img src="assets/_icon/insta.jpg" />
+                </div>
+            </a>
+        </div>
+    </div>
---
+++ header.php
@@ -0,0 +1,17 @@
+<div class="header pc" id="header">
+    <a href="index.php">
+        <div class="menu_bt">Home</div>
+    </a>
+
+    <a href="about.php">
+        <div class="menu_bt">About</div>
+    </a>
+
+    <a href="works.php">
+        <div class="menu_bt">Works</div>
+    </a>
+
+    <a href="contact.php">
+        <div class="menu_bt">Contact</div>
+    </a>
+</div>
