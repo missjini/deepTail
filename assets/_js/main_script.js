c9a5bd5c888341f40480a4c28cd972cdaa62741f
missjini <kimhero91@naver.com>
2024-04-14 23:53:44 +0900

    클릭시 이미지 바꾸기 함수 추가

--- assets/_js/main_script.js
+++ assets/_js/main_script.js
@@ -27,3 +27,33 @@ function photoViewing(obj){
     document.getElementById("photo_viewer").style.backgroundImage = "url(" + obj.children[0].src + ")";
     document.getElementById("photo_viewer").style.width = document.getElementById("photo_viewer").offsetHeight;
 }
+
+const changeImage = (imageName) => {
+  document.getElementById("detail_studio_image").src='assets/_img/'+imageName+'.jpg';
+
+  if(imageName == 'pf2'){
+    document.getElementById("detail_studio_image").style.width='45px';
+  }
+  else{
+    document.getElementById("detail_studio_image").style.width='145px';
+  }
+  
+    
+  // if(number === 'one'){
+  //     console.log('진입',number)
+  //     console.log(document.getElementById("miss"))
+  //     document.getElementById("miss").src='assets/_img/pf1.jpg';
+  // }
+  // else if(number === 'two'){
+  //     console.log('진입',number)
+  //     document.getElementById("miss").src='assets/_img/pf2.jpg';
+  // }
+  // else if(number === 'three'){
+  //     console.log('진입',number)
+  //     document.getElementById("miss").src='assets/_img/pf3.jpg';
+  // }
+  // else{
+  //     console.log('진입',number)
+  //     document.getElementById("miss").src='assets/_img/pf4.jpg';
+  // }
+}
--- index.php
+++ index.php
@@ -53,6 +53,12 @@
     </header>
     <content class="index">
         <div class="layer1">
+<!-- 
+        <div onclick = "changeImage('pf1')">1</div>
+        <div onclick = "changeImage('pf2')">2</div>
+        <div onclick = "changeImage('pf3')">3</div>
+        <div onclick = "changeImage('pf4')">4</div>
+        <img id='detail_studio_image' src="assets/_img/pf1.jpg" alt="바위이미지" /> -->
             <div class="col1">
                 <div></div>
                 <div class="color_blocks">
@@ -310,6 +316,9 @@
 
                 </div>
             </div>
+
+
+           
         </div>
     </content>
     <footer>
@@ -318,3 +327,7 @@
 </body>
 
 </html>
+<script>
+
+
+</script>
